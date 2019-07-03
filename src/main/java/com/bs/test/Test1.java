54cc211702071ff6c59e555f1ef332c42a1cfc75
eclipse_user <eclipse_user@qq.com>
2019-07-03 22:09:18 +0800

    Test03 first commit

--- src/main/java/com/bs/test/Test1.java
+++ src/main/java/com/bs/test/Test1.java
@@ -2,7 +2,7 @@ package com.bs.test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("bbbbb");
-		System.out.println("aaaaa");
+		System.out.println("aaaa");
+		System.out.println("modify222");
 	}
 }
