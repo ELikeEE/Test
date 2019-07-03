0c4106c7c4c37e2a0bb7762032eac5420d7ce3a4
eclipse_user <eclipse_user@qq.com>
2019-07-03 13:47:24 +0800

    sss

--- src/main/java/com/bs/test/Test1.java
+++ src/main/java/com/bs/test/Test1.java
@@ -2,10 +2,7 @@ package com.bs.test;
 
 public class Test1 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("test11-----modify2222-----");
-=======
 		System.out.println("test11-----modify111-----");
->>>>>>> branch 'master' of https://github.com/ELikeEE/Test.git
 	}
 }
