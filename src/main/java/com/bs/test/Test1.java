7ec0d049d8e8643a415286f96c0a3d3dc19858a0
eclipse_user <eclipse_user@qq.com>
2019-07-03 18:29:03 +0800

    test03 commit1

--- src/main/java/com/bs/test/Test1.java
+++ src/main/java/com/bs/test/Test1.java
@@ -2,7 +2,6 @@ package com.bs.test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("test11-----modify2222-----");
-		System.out.println("test11-----modify111-----");
+		System.out.println("test11-----modify222-----");
 	}
 }
